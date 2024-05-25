30e5b13d3ec56c26f58ee374a5c8addc7a2866b0
Gustavo Biasi <gustavo.biasi@grupoimpulse.com.br>
2024-05-25 08:42:46 -0300

    first commit

---
+++ src/main/java/br/com/dio/desafioreactor/Question1.java
@@ -0,0 +1,16 @@
+package br.com.dio.desafioreactor;
+
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public class Question1 {
+
+    /*
+    Recebe uma lista de longs, incrementa 1 nos valores e retorna um flux dos resultados
+     */
+    public Flux<Long> inc(final List<Long> numbers){
+        return Flux.fromIterable(numbers)
+                .map(n -> n+1);
+    }
+}
